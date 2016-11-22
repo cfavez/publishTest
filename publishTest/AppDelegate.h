467932e32cac8ff9613e6401466d3255eb8b9c3a
Cendrine Favez <cendrine.favez@b-i.com>
2016-11-22 15:53:22 +0100

    First app delegate and project structure

---
+++ publishTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  publishTest
+//
+//  Created by Cendrine Favez on 22/11/16.
+//  Copyright © 2016 Blue Infinity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ publishTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  publishTest
+//
+//  Created by Cendrine Favez on 22/11/16.
+//  Copyright © 2016 Blue Infinity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
